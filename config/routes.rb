9a18367ebeaed949103206aeeedf676d366f99d4
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-04-30 05:02:06 +0900

    Create budget
    * Add Budget model
    * Add CRUD for Budget
    
    close #4

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@ require "locker_room/engine"
 require "locker_room/constraints/subdomain_required"
 
 Rails.application.routes.draw do
+  get 'budgets/index'
+
   constraints(LockerRoom::Constraints::SubdomainRequired) do
     resources :notes
+    resources :budgets
     root to: "desktop#index"
   end
 
