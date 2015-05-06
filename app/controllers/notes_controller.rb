ee619c50b0a1c3005cc72b1cff39928c904ce431
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-05-06 09:20:06 +0900

    Create paginator
    * Enter kaminari for pagination
    * Add pagination to `Notes#index`
    
    close #6

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -2,7 +2,7 @@ class NotesController < WorkspaceController
   before_action :load_note, :only => [:show, :edit, :update, :destroy]
 
   def index
-    @notes = Note.all
+    @notes = Note.order(:created_at => :desc).page(params[:page])
   end
 
   def new
