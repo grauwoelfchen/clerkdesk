4adf3c7a68078bab29d11487602b596e5d3ddd7a
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-05-26 04:53:50 +0900

    Add filtered note list by tag
    * Update note list style
    * Add filtered list view by tag parameter

---
+++ test/integration/routes/notes_test.rb
@@ -0,0 +1,50 @@
+require "test_helper"
+
+class NotesRouteTest < ActionDispatch::IntegrationTest
+  locker_room_fixtures(:accounts, :members, :users)
+  fixtures(:notes)
+
+  def test_route_to_notes
+    within_subdomain_host do |host|
+      assert_routing(
+        {method: "get", path: "#{host}notes"},
+        {controller: "notes", action: "index"})
+      assert_routing(
+        {method: "get", path: "#{host}notes?tag=ice"},
+        {controller: "notes", action: "index"})
+      assert_routing(
+        {method: "get",
+         path: "#{host}notes?column=title&direction=desc&tag=ice"},
+        {controller: "notes", action: "index"})
+      assert_routing(
+        {method: "get", path: "#{host}notes/new"},
+        {controller: "notes", action: "new"})
+      assert_routing(
+        {method: "post", path: "#{host}notes"},
+        {controller: "notes", action: "create"})
+      assert_routing(
+        {method: "get", path: "#{host}notes/1"},
+        {controller: "notes", action: "show", id: "1"})
+      assert_routing(
+        {method: "get", path: "#{host}notes/1/edit"},
+        {controller: "notes", action: "edit", id: "1"})
+      assert_routing(
+        {method: "patch", path: "#{host}notes/1"},
+        {controller: "notes", action: "update", id: "1"})
+      assert_routing(
+        {method: "put", path: "#{host}notes/1"},
+        {controller: "notes", action: "update", id: "1"})
+      assert_routing(
+        {method: "delete", path: "#{host}notes/1"},
+        {controller: "notes", action: "destroy", id: "1"})
+    end
+  end
+
+  private
+
+  def within_subdomain_host
+    user = locker_room_users(:oswald)
+    host = "http://#{user.account.subdomain}.example.org/"
+    yield(host)
+  end
+end
