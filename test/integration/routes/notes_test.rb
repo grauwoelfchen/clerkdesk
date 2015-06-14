952525589f15ca1b7ac2f3f3ab7cfd05b95de1d6
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-06-14 10:01:52 +0900

    Add people routing test

--- test/integration/routes/notes_test.rb
+++ test/integration/routes/notes_test.rb
@@ -2,41 +2,149 @@ require "test_helper"
 
 class NotesRouteTest < ActionDispatch::IntegrationTest
   locker_room_fixtures(:accounts, :members, :users)
-  fixtures(:notes)
+  fixtures(:notes, :people)
 
   def test_route_to_notes
     within_subdomain_host do |host|
-      assert_routing(
-        {method: "get", path: "#{host}/notes"},
-        {controller: "notes", action: "index"})
-      assert_routing(
-        {method: "get", path: "#{host}/notes?tag=ice"},
-        {controller: "notes", action: "index"})
-      assert_routing(
-        {method: "get",
-         path: "#{host}/notes?column=title&direction=desc&tag=ice"},
-        {controller: "notes", action: "index"})
-      assert_routing(
-        {method: "get", path: "#{host}/notes/new"},
-        {controller: "notes", action: "new"})
-      assert_routing(
-        {method: "post", path: "#{host}/notes"},
-        {controller: "notes", action: "create"})
-      assert_routing(
-        {method: "get", path: "#{host}/notes/1"},
-        {controller: "notes", action: "show", id: "1"})
-      assert_routing(
-        {method: "get", path: "#{host}/notes/1/edit"},
-        {controller: "notes", action: "edit", id: "1"})
-      assert_routing(
-        {method: "patch", path: "#{host}/notes/1"},
-        {controller: "notes", action: "update", id: "1"})
-      assert_routing(
-        {method: "put", path: "#{host}/notes/1"},
-        {controller: "notes", action: "update", id: "1"})
-      assert_routing(
-        {method: "delete", path: "#{host}/notes/1"},
-        {controller: "notes", action: "destroy", id: "1"})
+      assert_routing({
+        method: "get",
+        path:   "#{host}/notes"
+      }, {
+        controller: "notes",
+        action:     "index"
+      })
+      assert_routing({
+        method: "get",
+        path:   "#{host}/notes?tag=ice"
+      }, {
+        controller: "notes",
+        action:     "index"
+      })
+      assert_routing({
+        method: "get",
+        path:   "#{host}/notes?column=title&direction=desc&tag=ice"
+      }, {
+        controller: "notes",
+        action:     "index"
+      })
+      assert_routing({
+        method: "get",
+        path:   "#{host}/notes/new"
+      }, {
+        controller: "notes",
+        action:     "new"
+      })
+      assert_routing({
+        method: "post",
+        path:   "#{host}/notes"
+      }, {
+        controller: "notes",
+        action:     "create"
+      })
+      assert_routing({
+        method: "get",
+        path:   "#{host}/notes/1"
+      }, {
+        controller: "notes",
+        action:     "show", id: "1"
+      })
+      assert_routing({
+        method: "get",
+        path:   "#{host}/notes/1/edit"
+      }, {
+        controller: "notes",
+        action:     "edit",
+        id:         "1"
+      })
+      assert_routing({
+        method: "patch",
+        path:   "#{host}/notes/1"
+      }, {
+        controller: "notes",
+        action:     "update",
+        id:         "1"
+      })
+      assert_routing({
+        method: "put",
+        path:   "#{host}/notes/1"
+      }, {
+        controller: "notes",
+        action:     "update",
+        id:         "1"
+      })
+      assert_routing({
+        method: "delete",
+        path:   "#{host}/notes/1"
+      }, {
+        controller: "notes",
+        action:     "destroy",
+        id:         "1"
+      })
+    end
+  end
+
+  def test_route_to_people
+    within_subdomain_host do |host|
+      assert_routing({
+        method: "get",
+        path:   "#{host}/people"
+      }, {
+        controller: "people",
+        action:     "index"
+      })
+      assert_routing({
+        method: "get",
+        path:   "#{host}/people/new"
+      }, {
+        controller: "people",
+        action:     "new"
+      })
+      assert_routing({
+        method: "post",
+        path:   "#{host}/people"
+      }, {
+        controller: "people",
+        action:     "create"
+      })
+      assert_routing({
+        method: "get",
+        path:   "#{host}/people/1"
+      }, {
+        controller: "people",
+        action:     "show",
+        id:         "1"
+      })
+      assert_routing({
+        method: "get",
+        path:   "#{host}/people/1/edit"
+      }, {
+        controller: "people",
+        method:     "edit",
+        id:         "1"
+      })
+      assert_routing({
+        method: "patch",
+        path:   "#{host}/people/1"
+      }, {
+        controller: "people",
+        action:     "update",
+        id:         "1"
+      })
+      assert_routing({
+        method: "put",
+        path:   "#{host}/people/1"
+      }, {
+        controller: "people",
+        action:     "update",
+        id:         "1"
+      })
+      assert_routing({
+        method: "delete",
+        path:   "#{host}/people/1"
+      }, {
+
+
+      })
     end
   end
 
