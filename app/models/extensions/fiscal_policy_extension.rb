ad2e168d92bf541537beda1dc8dfa13c1202141e
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-07-27 11:27:52 +0900

    Fix fiscal object extension

--- app/models/extensions/fiscal_policy_extension.rb
+++ app/models/extensions/fiscal_policy_extension.rb
@@ -9,10 +9,12 @@ module FiscalPolicyExtension
   # https://github.com/rails/rails/blob/0edb6465971f7d937fce2bf0a8e1e2b540d56e0a/activemodel/lib/active_model/naming.rb#L146
   # http://api.rubyonrails.org/classes/ActiveModel/Naming.html#method-i-model_name
   def model_name
-    # pass namespage 'Finance'
-    namespace = OpenStruct.new
-    namespace.name = self.name.sub("::#{self.class_name}", "")
-    @_model_name = ActiveModel::Name.new(self, namespace)
+    @_model_name ||= begin
+      # pass namespage
+      namespace = OpenStruct.new
+      namespace.name = 'Finance'
+      ActiveModel::Name.new(self, namespace)
+    end
     super
   end
 end
