4fd8d4323acc61cacbfb352d3d76d7333b184487
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-08-09 01:03:02 +0900

    Fix users scope

--- app/models/locker_room/user.rb
+++ app/models/locker_room/user.rb
@@ -2,6 +2,10 @@ require "locker_room/concerns/models/user"
 
 class LockerRoom::User < ActiveRecord::Base
   include LockerRoom::Concerns::Models::User
+  include Sortable
+
+  paginates_per 10
+  sortable :username, :created_at, :updated_at
 
   has_one :identity, foreign_key: :user_id, class_name: "Identity"
   has_one :person, through: :identity, source: :user
