d55726f1ca0b15487a4329893061f69b8459dfe7
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2015-08-28 21:10:48 +0900

    Improve style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,6 +1,6 @@
-//= require semantic-ui/dist/semantic.min
 //= require jquery/dist/jquery.min
 //= require jquery-ujs
+//= require semantic-ui/dist/semantic.min
 //= require moment/min/moment-with-locales.min
 //= require autoNumeric/autoNumeric.js
 //= require_tree ./shared
