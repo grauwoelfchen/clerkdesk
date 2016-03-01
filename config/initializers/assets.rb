fc0a194664e85975dadaf32abaa818e29d34af59
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2016-03-01 19:45:37 +0900

    Add fonts into assets precompile path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,6 +10,7 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 Rails.application.config.assets.precompile += %w(
   font-awesome/fonts/*
+  semantic-ui/dist/themes/default/fonts/*
   jquery-tag-editor/delete.*
   src/**/*
   src/*
