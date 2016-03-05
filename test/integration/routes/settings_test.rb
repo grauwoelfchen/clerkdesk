2afe59a091983d4a230da67aa5d54fbf3467f3f7
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2016-03-05 20:40:47 +0900

    Add change password

--- test/integration/routes/settings_test.rb
+++ test/integration/routes/settings_test.rb
@@ -1,37 +1,73 @@
 require 'test_helper'
 
-class SettingsRouteTest < ActionDispatch::IntegrationTest 
+class SettingsRouteTest < ActionDispatch::IntegrationTest
   locker_room_fixtures(:teams, :users, :mateships)
   fixtures(:'finance/ledgers', :'finance/budgets', :'finance/accounts',
            :'finance/entries', :'finance/categories', :'finance/categories')
 
-  def test_route_to_finance_ledgers
+  def test_route_to_users
     within_subdomain_host do |host|
-      assert_routing({
-        method: 'get',
-        path:   "#{host}/settings/account"
-      }, {
-        controller: 'settings/users',
-        action:     'edit'
-      })
-      assert_routing({
-        method: 'patch',
-        path:   "#{host}/settings/account"
-      }, {
-        controller: 'settings/users',
-        action:     'update'
-      })
-      assert_routing({
-        method: 'put',
-        path:   "#{host}/settings/account"
-      }, {
-        controller: 'settings/users',
-        action:     'update'
-      })
+      at_locker_room do
+        assert_routing({
+          method: 'get',
+          path:   "#{host}/settings/user"
+        }, {
+          controller: 'locker_room/settings/users',
+          action:     'edit'
+        })
+        assert_routing({
+          method: 'patch',
+          path:   "#{host}/settings/user"
+        }, {
+          controller: 'locker_room/settings/users',
+          action:     'update'
+        })
+        assert_routing({
+          method: 'put',
+          path:   "#{host}/settings/user"
+        }, {
+          controller: 'locker_room/settings/users',
+          action:     'update'
+        })
+      end
+    end
+  end
+
+  def test_route_to_passwords
+    within_subdomain_host do |host|
+      at_locker_room do
+        assert_routing({
+          method: 'get',
+          path:   "#{host}/settings/password/edit"
+        }, {
+          controller: 'locker_room/settings/passwords',
+          action:     'edit'
+        })
+        assert_routing({
+          method: 'patch',
+          path:   "#{host}/settings/password"
+        }, {
+          controller: 'locker_room/settings/passwords',
+          action:     'update'
+        })
+        assert_routing({
+          method: 'put',
+          path:   "#{host}/settings/password"
+        }, {
+          controller: 'locker_room/settings/passwords',
+          action:     'update'
+        })
+      end
     end
   end
 
   private
+
+  def at_locker_room
+    @routes = LockerRoom::Engine.routes
+    yield
+    @routes = Rails.application.routes
+  end
 
   def within_subdomain_host
     user = locker_room_users(:oswald)
