67877e59b8ced71684c81f621b04b397f4bddbb3
Yasuhiro Asaka <yasuhiro.asaka@grauwoelfchen.net>
2017-01-07 09:32:07 +0900

    Remove desktop

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,7 @@ Rails.application.routes.draw do
       constraints: {code: /[A-Z]{2}/},
       defaults:    {format: :json}
 
-    root to: 'desktop#index'
+    root to: 'finance/ledgers#index'
   end
 
   constraints(Constraints::WithoutSubdomain) do
